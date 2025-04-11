dfe2db8027bdb3cac5d5f343ad56c29d24f49083
dydwns6837 <dydwns6837@naver.com>
2025-04-11 18:18:26 +0900

    frontend branch 생성후 커밋

--- src/main/frontend/src/components/yongjun/Yongjun.jsx
+++ src/main/frontend/src/components/yongjun/Yongjun.jsx
@@ -1,10 +1,11 @@
 const Yongjun = () => {
   return (
-    <>
-      <div>용준하이</div>
-      <div>용준하이</div>
-      <div>merge가 와있나요</div>
-    </>
+      <>
+          <div>용준하이</div>
+          <div>용준하이</div>
+          <div>merge가 와있나요</div>
+          <div>frontend branch생성</div>
+      </>
   )
 }
 export default Yongjun
