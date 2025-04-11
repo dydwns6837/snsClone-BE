7d3631921e2f7a64bad9aa889551ded84e58772f
Yuno Kim <unokim1547@gmail.com>
2025-04-11 18:22:34 +0900

    frontend가져오고 나서 push

--- src/main/frontend/src/components/yongjun/Yongjun.jsx
+++ src/main/frontend/src/components/yongjun/Yongjun.jsx
@@ -5,6 +5,7 @@ const Yongjun = () => {
           <div>용준하이</div>
           <div>merge가 와있나요</div>
           <div>frontend branch생성</div>
+          <div>frontend branch 가져오고 커밋생성</div>
       </>
   )
 }
